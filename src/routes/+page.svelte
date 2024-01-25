753ab7025dde0bf011fe0b37f1882472d7518aa5
Vlad Sunny <vladislavsunny@gmail.com>
2024-01-25 16:23:34 +0300

    Buttons fix
    Removing gray background when pressing buttons on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,17 @@
 
 </script>
 
+<style>
+	button {
+		-webkit-tap-highlight-color: rgba(0,0,0,0);
+		-webkit-tap-highlight-color: transparent;
+	}
+</style>
+
 <div class="h-screen w-screen flex items-center justify-center">
-	<button class='btn variant-filled mr-2'>Share</button>
+	<button class='btn variant-filled mr-2 focus:outline-none'>Share</button>
 	<button class='btn variant-filled-primary' on:click={handleClick}>clicks: {clicks}</button>
-	<button class='btn variant-filled-secondary ml-2'>SET</button>
+	<button class='btn variant-filled-secondary ml-2 bg-transparent outline-none border-none'>SET</button>
+	
+	  
 </div>
