84f9041ab6320cef77bbec2b22b04d4cb4f41f51
Vladislav Sungurov <vladislavsunny@gmail.com>
2024-02-01 21:39:37 +0300

    Fixes

--- src/routes/update-score/+server.ts
+++ src/routes/update-score/+server.ts
@@ -8,11 +8,25 @@ export const POST: RequestHandler = async ({request}) => {
     console.log(chatId, userId, gameMessageId, score)
     const bot = new TelegramBot(TOKEN);
 
-    bot.setGameScore(parseInt(userId), score, {
-        chat_id: parseInt(chatId),
-        message_id: parseInt(gameMessageId),
-        force: true
+    // bot.setGameScore(parseInt(userId), score, {
+    //     chat_id: parseInt(chatId),
+    //     message_id: parseInt(gameMessageId),
+    //     force: true
+    // })
+
+    let formData = new FormData();
+    formData.append('user_id', userId);
+    formData.append('chat_id', chatId);
+    formData.append('message_id', gameMessageId);
+    formData.append('force', 'true');
+    formData.append('score', score.toString());
+
+    const result = await fetch(`https://api.telegram.org/bot${TOKEN}/setGameScore`, {
+        method: 'POST',
+        body: formData
     })
+
+    console.log(result);
 
     return new Response();
 };
