4dba3e817104153923fb3a4e2fdd76bf5d953ae6
Vlad Sunny <vladislavsunny@gmail.com>
2024-02-02 08:14:54 +0300

    Multiplayer

--- src/routes/update-score/+server.ts
+++ src/routes/update-score/+server.ts
@@ -4,22 +4,45 @@ import type { UpdateScoreArgs } from './types';
 
 export const POST: RequestHandler = async ({request}) => {
     const {chatId, userId, gameMessageId, inlineMessageId, score} = await request.json() as UpdateScoreArgs;
-    console.log(chatId, userId, gameMessageId, score)
+    console.log(chatId, userId, gameMessageId, inlineMessageId, score);
 
     let formData = new FormData();
-    // formData.append('user_id', userId);
-    // formData.append('chat_id', chatId);
-    // formData.append('message_id', gameMessageId);
-    formData.append('inline_message_id', inlineMessageId)
-    formData.append('force', 'true');
+    formData.append('user_id', userId);
     formData.append('score', score.toString());
+    formData.append('force', 'true'); // Убедитесь, что знаете последствия использования этого параметра
 
-    const result = await fetch(`https://api.telegram.org/bot${TOKEN}/setGameScore`, {
-        method: 'POST',
-        body: formData
-    })
+    // Эти поля взаимоисключающие, добавляем соответствующее
+    if (inlineMessageId) {
+        formData.append('inline_message_id', inlineMessageId);
+    } else {
+        formData.append('chat_id', chatId);
+        formData.append('message_id', gameMessageId);
+    }
 
-    console.log(result);
+    try {
+        const response = await fetch(`https://api.telegram.org/bot${TOKEN}/setGameScore`, {
+            method: 'POST',
+            body: formData
+        });
 
-    return new Response();
+        const result = await response.json();
+        console.log(result);
+
+        // Возвращаем ответ клиенту
+        return new Response(JSON.stringify(result), {
+            status: response.ok ? 200 : 400, // 200 OK при успехе, иначе 400 Bad Request
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (error) {
+        // Обработка ошибок при запросе к API Telegram
+        console.error(error);
+        return new Response(JSON.stringify({error: 'Failed to set game score'}), {
+            status: 500, // 500 Internal Server Error
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
 };
