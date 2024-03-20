19cc61458ad04068d3bb8ee5bb84f89080265158
Vlad Sunny <vladislavsunny@gmail.com>
2024-03-20 08:14:23 +0300

    Fixes

--- src/routes/supaTest/+server.ts
+++ src/routes/supaTest/+server.ts
@@ -1,8 +1,8 @@
 import { supabase } from "$lib/supabaseClient";
 
-// export async function load() {
-//   const { data } = await supabase.from("donates").select('*');
-//   return {
-//     countries: data ?? [],
-//   };
-// }
+export async function _load() {
+  const { data } = await supabase.from("donates").select('*');
+  return {
+    countries: data ?? [],
+  };
+}
